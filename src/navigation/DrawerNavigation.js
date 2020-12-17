01d11a0251566950feaa4d997042f258082a0dd0
YusufSina <yusufsinayildiz@gmail.com>
2020-12-17 13:03:33 +0300

    added logout button to drawer

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -1,10 +1,28 @@
 import * as React from 'react'
-import { Button } from 'react-native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import { NavigationContainer } from '@react-navigation/native'
+import { Button, StyleSheet } from 'react-native'
+import {
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem
+} from '@react-navigation/drawer'
 import { useDispatch } from 'react-redux'
 import { logout } from '../store/actions/authAction'
 import PrivateNavigation from './PrivateNavigation'
+
+function CustomDrawerContent(props) {
+    const dispatch = useDispatch();
+
+    const logOut = () => {
+        dispatch(logout())
+    }
+    return (
+      <DrawerContentScrollView {...props} contentContainerStyle={style.content}>
+        <DrawerItemList {...props} />
+        <DrawerItem label="Çıkış" onPress={() => logOut()} />
+      </DrawerContentScrollView>
+    );
+  }
 
 const Drawer = createDrawerNavigator()
 
@@ -16,8 +34,20 @@ export default function DrawerNavigation() {
     }
 
     return (
-            <Drawer.Navigator initialRouteName="Home">
-                <Drawer.Screen name="Home" component={PrivateNavigation} />
-            </Drawer.Navigator>
+        <Drawer.Navigator 
+            initialRouteName="Home" 
+            drawerContent={props => <CustomDrawerContent {...props} />}
+            drawerStyle={style.drawer}
+        >
+            <Drawer.Screen name="Ana sayfa" component={PrivateNavigation} />
+        </Drawer.Navigator>
     )
 }
+
+const style = StyleSheet.create({
+    content: {
+        flex: 1,  
+        flexDirection: 'column', 
+        justifyContent: 'space-between'
+    }, 
+});
